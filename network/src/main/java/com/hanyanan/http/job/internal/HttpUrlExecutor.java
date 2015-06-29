873265a85202b55009e87d7dc15ef7c9f42d45aa
hanyanan <onesubone@gmail.com>
2015-06-29 23:40:40 +0800

    backup

---
+++ network/src/main/java/com/hanyanan/http/job/internal/HttpUrlExecutor.java
@@ -0,0 +1,11 @@
+package com.hanyanan.http.job.internal;
+
+import com.hanyanan.http.internal.HttpResponse;
+import com.hanyanan.http.job.HttpRequestJob;
+import com.hyn.job.JobExecutor;
+
+/**
+ * Created by n550 on 2015/6/29.
+ */
+public class HttpUrlExecutor implements JobExecutor<HttpRequestJob, HttpResponse> {
+}
